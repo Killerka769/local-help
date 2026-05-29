a31e22dd1f690ad8e4b323216c398f8611c23bcd
Killerka769 <gaydar.123@mail.ru>
2026-05-29 20:52:55 +0300

    temp: deploy

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,35 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   // reactCompiler: true,
-  compress: true,
-  distDir: '.next',
-  generateEtags: false,
+  // compress: true
 };
 
 export default nextConfig;
+
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   compress: true,
+//   distDir: '.next',
+//   // Отключаем HTTP/2, принудительно используем HTTP/1.1
+//   httpAgentOptions: {
+//     keepAlive: false,
+//   },
+//   // Добавляем заголовки для совместимости
+//   async headers() {
+//     return [
+//       {
+//         source: '/:path*',
+//         headers: [
+//           {
+//             key: 'Connection',
+//             value: 'close',
+//           },
+//         ],
+//       },
+//     ];
+//   },
+// };
+
+
+// export default nextConfig;
