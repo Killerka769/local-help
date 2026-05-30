7caeb79140de7787b179e12398669b94b2003d82
Killerka769 <gaydar.123@mail.ru>
2026-05-30 23:16:20 +0300

    temp: deploy

--- next.config.ts
+++ next.config.ts
@@ -1,37 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  // reactCompiler: true,
-  // compress: true
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
-
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   compress: true,
-//   distDir: '.next',
-//   // Отключаем HTTP/2, принудительно используем HTTP/1.1
-//   httpAgentOptions: {
-//     keepAlive: false,
-//   },
-//   // Добавляем заголовки для совместимости
-//   async headers() {
-//     return [
-//       {
-//         source: '/:path*',
-//         headers: [
-//           {
-//             key: 'Connection',
-//             value: 'close',
-//           },
-//         ],
-//       },
-//     ];
-//   },
-// };
-
-
-// export default nextConfig;
